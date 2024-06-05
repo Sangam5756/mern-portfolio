13bf74385afb3379eb11fb1bf6cca96d42c2e507
sangam5756 <sangammunde3@gmail.com>
2024-06-05 20:41:18 +0530

    static part is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
+import { useState } from "react";
+import Loader from "./components/Loader";
+
 export default function App() {
+  const [Loading, setLoading] = useState(false);
   return (
     <>
       <BrowserRouter>
+      {Loading ? <Loader/> : null}
         <Routes>
           <Route  path="/" element={<Home/>}/>
           
